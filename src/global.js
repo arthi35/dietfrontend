4da2d37db77e5e868e99b59fee714535c82255ae
arthi35 <arthimba35@gmail.com>
2022-12-19 13:55:19 +0530

    render

--- src/global.js
+++ src/global.js
@@ -1,2 +1,4 @@
 // export const API="https://6209ed7192946600171c55be.mockapi.io"
-export const API="https://diet001.herokuapp.com";
+// export const API="https://diet001.herokuapp.com";
+export const API="https://dietnode.onrender.com";
+//https://dietnode.onrender.com/
